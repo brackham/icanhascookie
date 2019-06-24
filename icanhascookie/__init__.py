60b9b067dcd0a242cbb9199ae1179da24c24b925
brackham <brackham@email.arizona.edu>
2019-06-24 14:46:41 -0700

    Add all from core.py to namespace

--- icanhascookie/__init__.py
+++ icanhascookie/__init__.py
@@ -26,10 +26,9 @@ if LooseVersion(sys.version) < LooseVersion(__minimum_python_version__):
 
 if not _ASTROPY_SETUP_:   # noqa
     # For egg_info test builds to pass, put package imports here.
-    from .example_mod import *   # noqa
+    # from .example_mod import *   # noqa
     from .core import *
-    from . import ask
     # Then you can be explicit to control what ends up in the namespace,
-    __all__ += ['do_primes']   # noqa
+    # __all__ += ['do_primes']   # noqa
     # or you can keep everything from the subpackage with the following instead
-    # __all__ += example_mod.__all__
+    __all__ += core.__all__
