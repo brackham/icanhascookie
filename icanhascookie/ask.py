ef108adf988e025705dd86dbb6b7e2c725493b69
brackham <brackham@email.arizona.edu>
2019-06-24 11:22:09 -0700

    Create ask.py

---
+++ icanhascookie/ask.py
@@ -0,0 +1,21 @@
+__all__ = ['cookies']
+
+
+def cookies(n):
+    """
+    Can I have n cookies?
+
+    Parameters
+    ----------
+    n: int
+        The number of cookies.
+
+    Returns
+    -------
+    result: list
+        The list of cookies.
+    """
+
+    result = ['cookie']*n
+
+    return result
